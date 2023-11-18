91f1cc8c6512fb7578a28d341b5acbefa63c9b09
Daniil Utkin <d.utkin@tesonero.com>
2023-11-18 04:46:09 +0300

    Initial commit

---
+++ src/__tests__/LoginForm.test.tsx
@@ -0,0 +1,11 @@
+import {
+  render, // Рендерит компоненты и возвращает вспомогательные методы
+  fireEvent, // Симулирует DOM events
+  waitForElement, // Ожидает изменения в пользовательском интерфейсе
+} from "@testing-library/react";
+
+describe('<LoginForm />', () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+
+  });
+});
